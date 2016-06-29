e8bd8dd72536a93a17d0382d664f5d1113f1076c
psachdev6375 <puneet.sachdev@gmail.com>
2016-06-29 15:01:27 +0530

    Updated install shell script

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,2 +1,6 @@
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install -y docker.io 
+sudo -s 
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+usermod -G docker vagrant
